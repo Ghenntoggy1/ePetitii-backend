13f880de950894d693f2632cb2595fd1f972837d
Ghenntoggy1 <roman.gusev@isa.utm.md>
2023-11-18 19:45:14 +0200

    fix

--- src/main/java/org/example/Location/LocationRepository.java
+++ src/main/java/org/example/Location/LocationRepository.java
@@ -2,5 +2,5 @@ package org.example.Location;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
 }
