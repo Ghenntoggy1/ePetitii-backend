97b7dcc2f17d4bc03564c925c45be9ddc3458252
Felicia <felicianovac4@gmail.com>
2023-11-18 14:52:11 +0200

    create class Receiver

---
+++ src/main/java/org/example/receiver/Receiver.java
@@ -0,0 +1,29 @@
+package org.example.receiver;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "Receiver")
+public class Receiver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "receiver_id")
+    private Integer receiver_id;
+
+    @Column(name = "receiver_name_ro")
+    private String receiver_name_ro;
+
+    @Column(name = "receiver_name_en")
+    private String receiver_name_en;
+
+    @Column(name = "receiver_name_ru")
+    private String receiver_name_ru;
+}
